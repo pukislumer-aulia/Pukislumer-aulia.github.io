8e8baafe00ebf571637ac63be67e3c38f100b3a3
pukislumer-aulia <romyshimorinfu002@gmail.com>
2025-04-29 05:46:46 +0700

    Update and rename admin.js to main.js

--- main.js
+++ main.js
@@ -8,9 +8,10 @@ function loadTestimonials() {
     const div = document.createElement('div');
     div.className = 'testimonial-item';
     div.innerHTML = `
-      <div class="testimonial-name">${item.name}</div>
+      <div class="testimonial-name"><b>${item.name}</b></div>
       <div class="testimonial-text">"${item.testimonial}"</div>
-      <button class="delete-btn" onclick="deleteTestimonial(${index})">Hapus</button>
+      <button onclick="editTestimonial(${index})">Edit</button>
+      <button onclick="deleteTestimonial(${index})">Hapus</button>
     `;
     testimonialList.appendChild(div);
   });
@@ -22,8 +23,18 @@ function loadTestimonials() {
 
 function deleteTestimonial(index) {
   let testimonials = JSON.parse(localStorage.getItem('testimonials')) || [];
-  if (confirm('Yakin mau hapus?')) {
+  if (confirm('Yakin ingin menghapus?')) {
     testimonials.splice(index, 1);
+    localStorage.setItem('testimonials', JSON.stringify(testimonials));
+    loadTestimonials();
+  }
+}
+
+function editTestimonial(index) {
+  let testimonials = JSON.parse(localStorage.getItem('testimonials')) || [];
+  let newTestimonial = prompt("Edit testimoni:", testimonials[index].testimonial);
+  if (newTestimonial !== null) {
+    testimonials[index].testimonial = newTestimonial;
     localStorage.setItem('testimonials', JSON.stringify(testimonials));
     loadTestimonials();
   }
