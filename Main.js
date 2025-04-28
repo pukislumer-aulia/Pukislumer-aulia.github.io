9653da116fa8337688669fda0949d3b827f51b5c
pukislumer-aulia <romyshimorinfu002@gmail.com>
2025-04-28 17:33:29 +0700

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,25 +1,11 @@
 function toggleToppingAtas() {
-  var tipeTopping = document.getElementById("tipeTopping").value;
-  var toppingAtasSection = document.getElementById("toppingAtasSection");
-  if (tipeTopping === "Double") {
-    toppingAtasSection.style.display = "block";
-  } else {
-    toppingAtasSection.style.display = "none";
-  }
+  var tipe = document.getElementById('tipeTopping').value;
+  var toppingAtas = document.getElementById('toppingAtasSection');
+  toppingAtas.style.display = (tipe === 'Single') ? 'none' : 'block';
 }
 
-document.getElementById('testimonialForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  var name = document.getElementById('nameInput').value;
-  var testimonial = document.getElementById('testimonialInput').value;
-  
-  if (name && testimonial) {
-    var testimonialList = document.getElementById('testimonialsList');
-    var newTestimonial = document.createElement('p');
-    newTestimonial.innerHTML = `<strong>${name}</strong>: ${testimonial}`;
-    testimonialList.appendChild(newTestimonial);
-    
-    document.getElementById('nameInput').value = '';
-    document.getElementById('testimonialInput').value = '';
-  }
-});
+window.onload = function() {
+  document.getElementById('loader').style.display = 'none';
+  document.querySelector('.page-wrapper').style.display = 'block';
+  toggleToppingAtas();
+};
